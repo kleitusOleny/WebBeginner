fe692ba386e2a3a8dd3aeeef95ef52ba7385946e
Oleny <Olenydev@gmail.com>
2024-09-25 12:40:53 +0700

    Math Object

---
+++ JavaScriptCourse/Math_Object/index.js
@@ -0,0 +1,36 @@
+let x = 3.2;
+let y = 5.1;
+let z;
+
+z = Math.round(x);
+console.log("Round: " + z);//round if >0.5 ? + 1 : -1
+
+z = Math.floor(y);
+console.log("floor: " + z);//floor alway eliminate number after dot
+
+z = Math.ceil(y);
+console.log("ceil: " + z);//ceil alway eliminate number after dot and + 1
+
+z = Math.trunc(y);
+console.log("trunc: " + z);//trunc alway eliminate number after dot
+
+z = Math.pow(2,4);
+console.log("pow: " + z);//pow same Exp
+
+z = Math.sin(45);
+console.log("Sin: " + z);
+
+z = Math.cos(45);
+console.log("Cos: " + z);
+
+z = Math.tan(45);
+console.log("Tan: " + z);
+
+z = Math.sign(45);
+console.log("Sign: " + z);
+
+z = Math.sign(-45);
+console.log("Sin: " + z);
+
+
+
